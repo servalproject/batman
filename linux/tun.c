358f8133f27d72c6151340d5a0e91a65289edcae
marek <marek@45894c77-fb22-0410-b583-ff6e7d5dbf6c>
2007-09-29 15:14:39 +0000

    rename tunnel interfaces tunX to gateX
    git-svn-id: http://downloads.open-mesh.org/svn/batman/trunk/batman@663 45894c77-fb22-0410-b583-ff6e7d5dbf6c

--- linux/tun.c
+++ linux/tun.c
@@ -79,7 +79,9 @@ int8_t add_dev_tun( struct batman_if *batman_if, uint32_t tun_addr, char *tun_de
 	/* set up tunnel device */
 	memset( &ifr_tun, 0, sizeof(ifr_tun) );
 	memset( &ifr_if, 0, sizeof(ifr_if) );
+
 	ifr_tun.ifr_flags = IFF_TUN | IFF_NO_PI;
+	strncpy( ifr_tun.ifr_name, "gate%d", IFNAMSIZ );
 
 	if ( ( *fd = open( "/dev/net/tun", O_RDWR ) ) < 0 ) {
 
